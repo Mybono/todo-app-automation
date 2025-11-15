8743860dd83ddaa9216d2f9045c5f2ee9acf6160
MyBono <besttest2you@gmail.com>
2025-11-15 18:21:55 +0200

    fixed

--- src/tests/example.test.ts
+++
@@ -1,5 +0,0 @@
-describe("Simple test", () => {
-  it("should run", () => {
-    console.log("✅ Test executed");
-  });
-});
---
+++ src/tests/main.test.ts
@@ -0,0 +1,26 @@
+import { timeout } from "../utils";
+import { screens } from "../screens";
+
+describe("Todo App", () => {
+
+  it("should add a task", async () => {
+    await screens.main.addTask(
+      "Buy milk",
+      "Remember to buy milk from the store",
+    );
+  });
+  it("should edit a task", async () => {
+    const titleSelector = await screens.main.addTask();
+    await screens.addEdit.editTask({ titleSelector: titleSelector });
+    const push = await driver!.$(screens.main.pushTaskSaved);
+    await push.waitForDisplayed({ timeout: timeout.elementAppear });
+    expect(await push.isDisplayed()).toBe(true);
+  });
+  it("should delete a task", async () => {
+    const titleSelector = await screens.main.addTask();
+    await screens.addEdit.deleteTask(titleSelector);
+    const push = await driver!.$(screens.main.pushTaskDeleted);
+    await push.waitForDisplayed({ timeout: timeout.elementAppear });
+    expect(await push.isDisplayed()).toBe(true);
+  });
+});
